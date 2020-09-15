55fbd25acaf3b206e6d0788fccc402140f4be8aa
Maikel van de Lisdonk <maikel@devhelpr.com>
2020-09-15 18:19:43 +0200

    added all functions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
 import { createExpressionTree } from './components/expression-parser';
-import { executeExpressionTree } from './components/expression-tree-execute';
 
-export { createExpressionTree, executeExpressionTree };
+import {
+  clearExpressionFunctions,
+  executeExpressionTree,
+  extractValueParametersFromExpressionTree,
+  isExpressionFunction,
+  registerExpressionFunction,
+} from './components/expression-tree-execute';
+
+import {
+  convertGridToNamedVariables,
+  isRangeValue,
+  getRangeFromValues,
+  getRangeValueParameters,
+} from './utils/grid-values';
+
+export {
+  clearExpressionFunctions,
+  convertGridToNamedVariables,
+  createExpressionTree,
+  executeExpressionTree,
+  extractValueParametersFromExpressionTree,
+  isExpressionFunction,
+  isRangeValue,
+  getRangeFromValues,
+  getRangeValueParameters,
+  registerExpressionFunction,
+};
